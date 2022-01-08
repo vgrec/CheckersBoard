5e5d0643f7d460a5a33df6229559281bbd28c231
Veaceslav Grec <grec.vs@gmail.com>
2022-01-08 20:07:08 +0100

    Create initial board

--- app/src/main/java/com/vgrec/checkersboard/MainActivity.kt
+++ app/src/main/java/com/vgrec/checkersboard/MainActivity.kt
@@ -10,12 +10,8 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -23,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,29 +39,80 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+enum class Piece {
+    WHITE,
+    BLACK,
+}
+
+data class Square(
+    val color: Color = Color.Red,
+    val piece: Piece? = null,
+)
+
 @ExperimentalFoundationApi
 @Composable
 fun CheckersBoards(name: String) {
-    val row = 8
+    val blackSquareColor = Color.Green
+    val whiteSquareColor = Color.White
+    val rows = 8
     val cols = 8
-    val board = Array(row) { IntArray(cols) }
 
-    val items: List<String> = (0..7).map { "$it" }
+    val initialWhitePiecePositions = listOf(
+        Pair(0, 1), Pair(0, 3), Pair(0, 5), Pair(0, 7),
+        Pair(1, 0), Pair(1, 2), Pair(1, 4), Pair(1, 6),
+        Pair(2, 1), Pair(2, 3), Pair(2, 5), Pair(2, 7)
+    )
+
+    val initialBlackPiecePositions = listOf(
+        Pair(5, 0), Pair(5, 2), Pair(5, 4), Pair(5, 6),
+        Pair(6, 1), Pair(6, 3), Pair(6, 5), Pair(6, 7),
+        Pair(7, 0), Pair(7, 2), Pair(7, 4), Pair(7, 6),
+    )
+
+    val board = Array(rows) { Array(cols) { Square() } }
+    (0 until rows).forEach { row ->
+        (0 until cols).forEach { col ->
+            val color = if (row % 2 == 0) {
+                if (col % 2 == 0) whiteSquareColor else blackSquareColor
+            } else {
+                if (col % 2 != 0) whiteSquareColor else blackSquareColor
+            }
+
+            val currentPosition = Pair(row, col)
+            val piece: Piece? = when {
+                initialWhitePiecePositions.contains(currentPosition) -> Piece.WHITE
+                initialBlackPiecePositions.contains(currentPosition) -> Piece.BLACK
+                else -> null
+            }
+
+            board[row][col] = Square(
+                color = color,
+                piece = piece
+            )
+        }
+    }
+
+
+    val items: List<String> = (0 until rows).map { "$it" }
     LazyColumn {
         itemsIndexed(items) { rowIndex: Int, item: String ->
             Row {
-                (0..7).map { colIndex: Int ->
-                    val color = if (rowIndex % 2 == 0) {
-                        if (colIndex % 2 == 0) Color.White else Color.Gray
-                    } else {
-                        if (colIndex % 2 != 0) Color.White else Color.Gray
+                (0 until cols).map { colIndex: Int ->
+                    val square: Square = board[rowIndex][colIndex]
+                    var pieceSymbol = ""
+                    square.piece?.let { piece ->
+                        pieceSymbol = if (piece == Piece.WHITE) {
+                            "w"
+                        } else {
+                            "b"
+                        }
                     }
 
                     Text(
                         textAlign = TextAlign.Center,
-                        text = AnnotatedString("$item:$colIndex"),
+                        text = AnnotatedString(pieceSymbol),
                         modifier = Modifier
-                            .background(color = color)
+                            .background(color = square.color)
                             .width(40.dp)
                             .height(40.dp)
                             .clickable {
