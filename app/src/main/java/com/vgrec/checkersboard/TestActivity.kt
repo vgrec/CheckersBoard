2cee505667606ce866bac508e50c1091ee6937fc
Veaceslav Grec <grec.vs@gmail.com>
2022-03-15 19:59:41 +0100

    Create test popup

--- app/src/main/java/com/vgrec/checkersboard/TestActivity.kt
+++ app/src/main/java/com/vgrec/checkersboard/TestActivity.kt
@@ -1,12 +1,23 @@
 package com.vgrec.checkersboard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.vgrec.checkersboard.ui.theme.CheckersBoardTheme
 
 class TestActivity : AppCompatActivity() {
@@ -15,15 +26,69 @@ class TestActivity : AppCompatActivity() {
 
         setContent {
             CheckersBoardTheme {
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(
+                    color = MaterialTheme.colors.background) {
+                    val openDialog = rememberSaveable {
+                        mutableStateOf(false)
+                    }
+
                     Button(
-                        onClick = {}
+                        modifier = Modifier.padding(8.dp),
+                        onClick = {
+                            openDialog.value = true
+                        }
                     ) {
                         Text(text = "HelloWorld")
+                    }
+
+                    if (openDialog.value) {
+                        SettingsPopup(onDismiss = {
+                            openDialog.value = false
+                        })
                     }
                 }
             }
         }
+    }
+
+    @Composable
+    private fun SettingsPopup(onDismiss: () -> Unit) {
+        AlertDialog(
+            modifier = Modifier.fillMaxWidth(),
+            onDismissRequest = { onDismiss() },
+            title = {
+                Text(text = "Settings")
+            },
+            text = {
+                Column {
+                    Text(text = "row 1")
+                    Text(text = "row 2")
+                }
+            },
+            buttons = {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    Button(
+                        modifier = Modifier
+                            .weight(1f),
+                        onClick = { /*TODO*/ }) {
+                        Text(text = "Save")
+                    }
+
+                    Spacer(modifier = Modifier.padding(4.dp))
+
+                    Button(
+                        modifier = Modifier
+                            .weight(1f),
+                        onClick = { /*TODO*/ }) {
+                        Text(text = "Cancel")
+                    }
+                }
+            }
+        )
     }
 
 }
